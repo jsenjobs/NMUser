1dea04795500e7a6923b2c06b544133bedc1384e
jsen <1922279340@qq.com>
2017-05-04 18:24:10 +0800

    'dev'

--- develop.sh
+++ develop.sh
@@ -1,15 +1,16 @@
 #!/bin/bash  
 
 msg='develop'
-if ['--m' == $1 || '--m' == $1]; then
-	$msg = $2
-fi
-echo $msg
+p="--m"
+if [ "--m"x == "$1"x -o  "-m"x == "$1"x ] && [ $# -ge 2 ];
+then  
+    msg=$2  
+fi  
 
 echo 'git add start'
 git add *
 git add -A
 echo 'git add end'
 echo 'git commit start'
-git commit -m 'develop'
+git commit -m \'$msg\'
 echo 'git commit end'
